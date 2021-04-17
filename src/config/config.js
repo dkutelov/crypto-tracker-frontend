6703d0c2c1d23080c30773155d3f0a42dfc1cc8e
Dari <dari.k@abv.bg>
2021-04-17 11:15:04 +0300

    config to heroku backend

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
-// export const config = {
-//   apiUrl: 'https://blooming-basin-18460.herokuapp.com/api',
-// };
-
 export const config = {
-  apiUrl: 'http://localhost:4000/api',
+  apiUrl: 'https://blooming-basin-18460.herokuapp.com/api',
 };
+
+// export const config = {
+//   apiUrl: 'http://localhost:4000/api',
+// };
